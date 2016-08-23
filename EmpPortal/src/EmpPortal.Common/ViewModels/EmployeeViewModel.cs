589bb566797f019eac0765b0df1622a6727ea743
ask3023 <sravan.ask@gmail.com>
2016-08-22 21:13:33 -0400

    Common project added

---
+++ EmpPortal/src/EmpPortal.Common/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpPortal.Common.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public string PrimaryEmail { get; set; }
+        public string SecondaryEmail { get; set; }
+    }
+}
--- EmpPortal/src/EmpPortal.Web/Controllers/AccountController.cs
+++ EmpPortal/src/EmpPortal.Web/Controllers/AccountController.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using EmpPortal.Common.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MVCApp.Controllers
 {
-    public class EmployeeController : Controller
+    public class AccountController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewData["Data"] = "Add employee here...";
-            return View("AddEmployee");
-        }
-
-        [HttpPost]
-        public IActionResult Add(EmployeeViewModel employeeViewModel)
-        {
-            
-            return View("AddEmployee");
+            return View();
         }
     }
 }
