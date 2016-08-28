6e99bd0e67d3ed66d16556a97b0a3446bad1248a
ask3023 <sravan.ask@gmail.com>
2016-08-28 18:54:33 -0400

    Model validations added

--- EmpPortal/src/EmpPortal.Common/ViewModels/EmployeeViewModel.cs
+++ EmpPortal/src/EmpPortal.Common/ViewModels/EmployeeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,21 @@ namespace EmpPortal.Common.ViewModels
 {
     public class EmployeeViewModel
     {
+        [Required(ErrorMessage = "First name is mandatory field.")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Date of birth")]
         public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Date of joining")]
         public DateTime DateOfJoining { get; set; }
+        [Required]
+        [Display(Name = "Primary Email")]
         public string PrimaryEmail { get; set; }
+        [Display(Name = "Secondary Email")]
         public string SecondaryEmail { get; set; }
     }
 }
